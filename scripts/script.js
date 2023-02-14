4d1da7e9c4a534ed8d6b21e1817361c0a35ebc97
Anastasia Bashkirova <bossmityab612@yandex.ru>
2023-02-15 01:17:45 +0300

    style: fixed list

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,17 @@
+// path — встроенный в Node.js модуль
+const path = require('path')
+
+module.exports = {
+  // Указываем путь до входной точки:
+  entry: './src/index.js',
+  // Описываем, куда следует поместить результат работы:
+  output: {
+    // Путь до директории (важно использовать path.resolve):
+    path: path.resolve(__dirname, 'dist'),
+    // Имя файла со сборкой:
+    filename: 'bundle.js'
+  }
+}
 
 // Открытие и закрытие меню
 const popup = document.querySelector('.header__navigation_cont-menu');
