7818a0cb3e934f428ee5de0eb03ac6f95d091def
Anastasia Bashkirova <bossmityab612@yandex.ru>
2023-02-15 18:16:33 +0300

    style: navigation

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,3 @@
-// path — встроенный в Node.js модуль
-const path = require('path')
-
-module.exports = {
-  // Указываем путь до входной точки:
-  entry: './src/index.js',
-  // Описываем, куда следует поместить результат работы:
-  output: {
-    // Путь до директории (важно использовать path.resolve):
-    path: path.resolve(__dirname, 'dist'),
-    // Имя файла со сборкой:
-    filename: 'bundle.js'
-  }
-}
-
 // Открытие и закрытие меню
 const popup = document.querySelector('.header__navigation_cont-menu');
 const openHeader = document.querySelector('.header__burger');
